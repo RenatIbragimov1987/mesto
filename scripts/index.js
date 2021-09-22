7b8a650c8dcb07f9a257d7d6d796a6f1795d5cab
Renat Ibragimov <naka070787@gmail.com>
2021-09-22 20:12:31 +0300

    commit

---
+++ scripts/index.js
@@ -0,0 +1,48 @@
+
+const popup = document.querySelector('.popup');
+const openButtonPopup =  document.querySelector('.profile__edit-button');
+const popupClose = document.querySelector('.popup__close-icon');
+const formElement = document.querySelector('.popup__container');
+const nameInput = document.querySelector('.popup__field_form_title');
+const jobInput = document.querySelector('.popup__field_form_subtitle');
+const profileTitle = document.querySelector('.profile__title');
+const profileParagraph = document.querySelector('.profile__paragraph');
+const submitButton = document.querySelector('.popup__submit-button');
+
+function openPopup() {
+    popup.classList.add('popup_opened');    
+}
+
+
+function closePopup() {
+    popup.classList.remove('popup_opened');    
+}
+popupClose.addEventListener('click', closePopup);
+
+
+function setPopupInputValue() {
+	nameInput.value = profileTitle.textContent;
+	jobInput.value = profileParagraph.textContent;
+	openPopup();
+}
+
+
+function setNodeTextValue() {
+	profileTitle.textContent = nameInput.value;
+	profileParagraph.textContent = jobInput.value;
+}
+
+
+function formSubmitHandler (e) {
+	e.preventDefault();
+	setNodeTextValue();
+	closePopup();
+}
+
+
+openButtonPopup.addEventListener('click', function() {
+	setPopupInputValue();
+});
+
+
+formElement.addEventListener('submit', formSubmitHandler);
